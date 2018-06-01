c9a9feec292b7f73dd066f3ac19cf35eb8ae5742
dan <danieleades@hotmail.com>
2018-06-01 18:28:16 +0100

    fixes

--- PhysicsModel.py
+++ PhysicsModel.py
@@ -5,23 +5,23 @@ class PhysicsModel_1D:
 
     def __init__(self):
         self.mass = 1
-        self.pos = 0
-        self.vel = 0   
+        self.position = 0
+        self.velocity = 0   
 
-    def _dx_dt(self,t,X,force):
-        # this is simply the jacobian of "F=ma"
+    def _dx_dt(self,X,force):
+        #jacobian
         x1, x2 = X
         x1dot = x2
         x2dot = force/self.mass
         return [x1dot, x2dot]
 
     def push(self,force,timestep):
-        X0 = [self.pos,self.vel]
+        X0 = [self.position,self.velocity]
         args=[]
         args.append(force)
-        fun=lambda t, y: self._dx_dt(t,y, *args)
+        fun=lambda t, y: self._dx_dt(y,force)
 
         #this uses an adaptive timestep to return an accurate numerical solution
         solution = solve_ivp(fun, (0,timestep), X0, method='RK45')
-        self.pos = solution.y[0][-1]
-        self.vel = solution.y[1][-1]
+        self.position = solution.y[0][-1]
+        self.velocity = solution.y[1][-1]
--- VehicleModel.py
+++ VehicleModel.py
@@ -3,14 +3,24 @@ from PhysicsModel import PhysicsModel_1D
 class VehicleModel:
     def __init__(self):
         self.model = PhysicsModel_1D()
-        self.maxForce = 1
+        self.max_force = 1
+        self.force_request = 0
+
+    def set_force_request(self,force):
+        if force<0:
+            force = max(force,-self.max_force)
+        else:
+            force = min(force,self.max_force)
+        self.force_request = force
+
+    def set_time_interval(self,time_interval):
+        self.time_interval = time_interval
     
-    def _limit_force(self,force):
-        return min(force,self.maxForce)
-    
-    def 
-        
-    def apply_force_request(self,force, timestep):
-        force = self._limit_force(force)
-        self.model.push(force, timestep)
-        
+    def run_step(self):
+        self.model.push(self.force_request,self.time_interval)
+
+    def get_position(self):
+        return self.model.position
+
+    def get_velocity(self):
+        return self.model.velocity
